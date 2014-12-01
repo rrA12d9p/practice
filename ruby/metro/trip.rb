20859ceb56541b2801b1a1955bae072dab450195
TREY <rkh.mymail@gmail.com>
2014-12-01 01:19:38 -0500

    reverting to 6cd086ed40c4302d12aeb85d14bfaf8a0c2a4deb

--- ruby/metro/trip.rb
+++ ruby/metro/trip.rb
@@ -6,16 +6,6 @@ class Trip
 		@dest = dest
 		@moves = []
 		@successful_paths = []
-		@min_path_length = 15
-		# @max_path_length = 35
-	end
-
-	def min_path_length
-		if @successful_paths.length == 0
-			return nil
-		else
-			return @successful_paths.map {|path| path.length}.min
-		end
 	end
 
 	# check every connection of every connection
@@ -23,8 +13,6 @@ class Trip
 	# don't go back to a stop in the current path
 
 	def map_paths(orig = @orig, dest = @dest, path = [])
-		# return if path.length > @max_path_length
-
 		# we have to duplicate each path array to its own object
 		# to ensure it isn't contaminated by another iteration
 		path = path.dup
@@ -33,9 +21,6 @@ class Trip
 		if orig == dest
 			if !@successful_paths.include?(path)
 				@successful_paths << path
-				if min_path_length < @min_path_length
-					@min_path_length = min_path_length 
-				end
 			end
 		else
 			# get connections
@@ -48,7 +33,7 @@ class Trip
 				# we have to do this to limit the paths generated
 				if @moves.include?({from: orig, to: c})
 					# allow paths to repeat initial stops
-					next unless path.length <= @min_path_length
+					next unless path.length < 5
 				end
 
 				@moves << {from: orig, to: c}
